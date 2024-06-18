a6fe218f023c7e48d8000ba67decf5ae5e921335
Руслан <xamet@icloud.com>
2024-06-18 23:41:11 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+# Задача 1
+print("привет, с добрым утром!")
+print(len("привет, с добрым утром!"))
+# Задача 2
+first = int(2)
+second = int(5)
+print(first + second)
+print(first - second)
+summa = int(first) + int(second)
+diff = int(first) - int(second)
+print(summa)
+print(diff)
+# Задача 3
+first = int(7)
+second = int(9)
+third = int(3)
+print(int(first + second + third) // 3)
+mean = int(first + second + third) // 3
+print(mean)
+# Задача 4
+first_string = str("Вторник")
+second_string = str("Понедельник")
+print(second_string, ',', first_string)
+# Задача 5
+a = int(3)
+b = int(7)
+c = int(5)
+f = int(36)
+print(int(a*b) + (a*c))
+print(int(f**3))
+print(46656//2)
+# Задача 6
+
+
+
+
+
